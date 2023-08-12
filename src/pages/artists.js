94273f18930628dae0c4954e5d84e411525d0103
Owen Buckley <owenbuckley13@gmail.com>
2023-08-12 15:51:59 -0400

    upgrade latest greenwood with enhanced styling

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -1,8 +1,9 @@
 import '../components/card.js';
+import { getArtists } from '../services/artists.js';
 
 export default class ArtistsPage extends HTMLElement {
   async connectedCallback() {
-    const artists = await fetch('https://www.analogstudios.net/api/artists').then(resp => resp.json());
+    const artists = await getArtists();
     const html = artists.map(artist => {
       const { name, imageUrl } = artist;
 
@@ -16,8 +17,13 @@ export default class ArtistsPage extends HTMLElement {
     }).join('');
 
     this.innerHTML = `
-      <h1>List of Artists: ${artists.length}</h1>
-      ${html}
+      <h2>SSR Page (w/ WCC)</h2>
+      <p>This is an example of a Greenwood SSR page route server-rendering Web Components; the same Card component used for the Fragments API demo on the home page in fact!  Greenwood is also statically bundling the share template (written in HTML) so that you can still share page templates even within SSR pages.</p>
+
+      <h3>List of Artists: ${artists.length}</h3>
+      <div class="artists-cards-contianer">
+        ${html}
+      </div>
     `;
   }
 }
